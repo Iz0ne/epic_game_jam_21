5f2e541eacf6647c291b0dde0e9c008922dd61f6
Iz0ne <matboardeurs@hotmail.com>
2021-07-11 16:50:06 +0200

    Rename character path finding class

--- Scripts/charactersPathFinding.cs
+++ Scripts/charactersPathFinding.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 
-public class charactersPathFinding : MonoBehaviour
+public class CharactersPathFinding : MonoBehaviour
 {
     //Animation
     Animator m_Animator;
